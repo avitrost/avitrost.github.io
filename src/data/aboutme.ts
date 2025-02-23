3375792d089cb7d3d984e17962113fec27973b2e
avitrost <avitrost@gmail.com>
2025-02-23 00:07:25 -0600

    reword about section

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -25,7 +25,7 @@ export const aboutMe: AboutMe = {
   description:
     "Hi! I am a first year Computer Science PhD student at <a href='https://www.wisc.edu'>University of Wisconsin–Madison</a> working with <a href='https://pages.cs.wisc.edu/~fredsala/'>Fred Sala</a> and <a href='https://tengyangxie.github.io/'>Tengyang Xie</a>. As an undergraduate at <a href='https://www.brown.edu/'>Brown University</a>, I was fortunate to work with <a href='https://cs.brown.edu/people/sbach/'>Stephen Bach</a>." +
     "<br><br>" +
-    "My research interests include synthetic data, data-centric ML, and reasoning. My recent emphasis has been on tailoring data for LLMs, and understanding the role that data plays. Feel free to reach out in you're interested in collaborating!",
+    "My research interests include synthetic data, data-centric ML, and reasoning. My recent emphasis has been on tailoring data for LLMs, and understanding the role that data plays. Feel free to reach out if you're interested in collaborating!",
   email: "astrost@wisc.edu",
   imageUrl:
     "/image_from_ios.jpg",
