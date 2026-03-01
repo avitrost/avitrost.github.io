c94369ee3d2e08fc46a4e385222ae56ae2509c38
avitrost <39007976+avitrost@users.noreply.github.com>
2026-03-01 17:18:54 -0600

    Update CV URL in aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -34,7 +34,7 @@ export const aboutMe: AboutMe = {
   linkedinUsername: "avi-trost",
   twitterUsername: "atrost3122",
   blogUrl: "https://",
-  cvUrl: "https://drive.google.com/file/d/1nXPj3a8PclDyJYdmQCvGuPoDaDcS9EdH/view?usp=sharing",
+  cvUrl: "https://drive.google.com/file/d/1PxE1MobP91Yv5MiWGk7znYRS4hpZPf6a/view?usp=sharing",
   institutionUrl: "https://www.wisc.edu",
   // altName: "",
   // secretDescription: "I like dogs.",
